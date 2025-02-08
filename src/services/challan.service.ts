5b497c51eb3d477f0765380d8b5e7465a8108a79
Gautam Malaviya <malaviyagautam0942@gmail.com>
2025-02-08 22:27:10 +0530

    fix the error in chllan servivce

--- src/services/challan.service.ts
+++ src/services/challan.service.ts
@@ -15,197 +15,6 @@ import { BillService } from "./bill.service.js";
 const customerService = new CustomerService();
 const billService = new BillService();
 export class ChallanService {
-    // async createChallan(challan: IChallan) {
-    //     const [existingChallan] = await Challan.aggregate([{
-    //         $match: {
-    //             custonerName: challan.custsomerName,
-    //             mobileNumber: challan.mobileNumber
-    //         }
-    //     }]);
-
-    //     if (existingChallan) {
-    //         throw new ApiError(statuscode.BADREQUEST, ERROR_MSG.EXISTS('Challan'));
-    //     }
-
-    //     challan.products = challan.products.map((p) => {
-    //         return {
-    //             date: p.date,
-    //             productName: p.productName,
-    //             size: p.size,
-    //             quantity: p.quantity,
-    //             rate: p.rate,
-    //             amount: p.amount,
-    //         }
-    //     })
-
-    //     const result = await Challan.create({
-    //         challanNumber: challan.challanNumber,
-    //         challenType: challan.type,
-    //         date: challan.date,
-    //         customerName: challan.customerName,
-    //         customerId: challan.customerId,
-    //         mobileNumber: challan.mobileNumber,
-    //         siteName: challan.siteName,
-    //         siteAddress: challan.siteAddress,
-    //         products: challan.products,
-    //         loading: challan.loading,
-    //         unloading: challan.unloading,
-    //         transportCharge: challan.transportCharge,
-    //         serviceCharge: challan.serviceCharge,
-    //         damageCharge: challan.damageCharge,
-    //         amount: challan.amount,
-    //         totalAmount: challan.totalAmount,
-    //     });
-
-    //     const bill = await Bills.findOne({
-    //         customerId: challan.customerId,
-    //         siteName: challan.siteName
-    //     })
-
-    //     const today = new Date();
-    //     const billProducts = challan.products.map((p) => {
-    //         return {
-    //             productName: p.productName,
-    //             size: p.size,
-    //             quantity: p.quantity || 0,
-    //             rate: p.rate || 0,
-    //             amount: p.amount || 0,
-    //             month: p.date?.getMonth(),
-    //             year: p.date?.getFullYear(),
-    //             previousRestBill: 0,
-    //             startingDate: p.date || today,
-    //             endingDate: null,
-    //             dayCount: Math.max(0, (today.getTime() - (p.date || today).getTime()) / (1000 * 3600 * 24) + 1)
-    //         }
-    //     });
-
-    //     const totalAmount = billProducts.reduce((accum, product) => {
-    //         return accum + (product.amount * product.dayCount);
-    //     }, 0);
-
-
-    //     let previousRestBill = 0;
-    //     const monthWiseData = [].map(() => {
-    //         const today = new Date();
-    //         let monthlyProducts: any = [];
-
-    //         billProducts.forEach((product) => {
-    //             const startDate = product.startingDate;
-    //             const endDate = challan.type == 'Return' ? challan.date : today;
-
-    //             const monthWiseAmounts = this.calculateMonthlyAmounts(startDate, endDate, product.rate, product.quantity);
-
-    //             monthWiseAmounts.forEach((monthData) => {
-    //                 const firstDayOfMonth = new Date(monthData.year, monthData.month - 1, 1);
-    //                 const lastDayOfMonth = new Date(monthData.year, monthData.month, 0);
-    //                 let productStartDate = firstDayOfMonth;
-    //                 let productEndDate = lastDayOfMonth;
-
-    //                 if (monthData.month === today.getMonth() + 1 && monthData.year === today.getFullYear()) {
-    //                     productEndDate = today;
-    //                 }
-
-    //                 productStartDate = productStartDate < new Date(product.startingDate) ? new Date(product.startingDate) : productStartDate;
-    //                 if (product.endingDate) {
-    //                     productEndDate = productEndDate > new Date(product.endingDate) ? new Date(product.endingDate) : productEndDate;
-    //                 }
-
-    //                 const dayCount = Math.max(0, (productEndDate.getTime() - productStartDate.getTime()) / (1000 * 3600 * 24) + 1);
-
-    //                 previousRestBill += monthData.amount;
-    //                 const remainingDue = previousRestBill;
-
-    //                 monthlyProducts.push({
-    //                     productName: product.productName,
-    //                     quantity: product.quantity,
-    //                     size: product.size,
-    //                     rate: product.rate,
-    //                     amount: monthData.amount,
-    //                     month: monthData.month,
-    //                     year: monthData.year,
-    //                     previousRestBill: remainingDue > 0 ? remainingDue : 0,
-    //                     startingDate: productStartDate,
-    //                     endingDate: productEndDate,
-    //                     dayCount: dayCount
-    //                 });
-    //             });
-    //         });
-
-    //         return monthlyProducts;
-    //     }).flat();
-
-
-    //     if (bill) {
-    //         const updatedProducts = bill.products;
-    //         billProducts.forEach((p)=>{
-    //             updatedProducts.push(p)
-    //         });
-
-    //         await Bills.findByIdAndUpdate(bill._id, {products : updatedProducts});
-
-    //         const getBill = await Bills.findOne({_id: bill._id});
-
-    //         const updatedBill = await Bills.findByIdAndUpdate(bill._id, {monthData : getBill?.monthData});
-
-
-    //     } else {
-    //         const createdBill = await Bills.create({
-    //             serviceCharge: 0,
-    //             damageCharge: 0,
-    //             products: billProducts,
-    //             totalPayment: totalAmount,
-    //             challans: [result._id],
-    //             monthData:  monthWiseData
-    //         })
-    //     }
-
-    //     return {
-    //         statuscode: statuscode.CREATED,
-    //         message: MSG.SUCCESS('Challan creation'),
-    //         data: result
-    //     };
-    // }
-
-    // private calculateMonthlyAmounts(startDate: Date, endDate: Date, rate: Number, quantity: Number) {
-    //     const monthWiseAmounts = [];
-
-    //     let currentMonth = new Date(startDate);
-    //     let currentYear = currentMonth.getFullYear();
-    //     let currentMonthIndex = currentMonth.getMonth();
-
-    //     while (currentMonth <= endDate) {
-    //         const monthStartDate = new Date(currentYear, currentMonthIndex, 1);
-    //         const nextMonthStartDate: any = new Date(currentYear, currentMonthIndex + 1, 1);
-    //         const monthEndDate = nextMonthStartDate > endDate ? endDate : new Date(nextMonthStartDate - 1); // End of the current month or the end date
-
-    //         const daysInMonth = Math.max(0, (monthEndDate.getTime() - monthStartDate.getTime()) / (1000 * 3600 * 24) + 1);
-
-    //         const amount = Number(rate) * Number(quantity) * daysInMonth;
-
-    //         monthWiseAmounts.push({
-    //             month: currentMonthIndex + 1,
-    //             year: currentYear,
-    //             amount: amount
-    //         });
-
-    //         currentMonthIndex++;
-    //         if (currentMonthIndex > 11) {
-    //             currentMonthIndex = 0;
-    //             currentYear++;
-    //         }
-    //         currentMonth = new Date(currentYear, currentMonthIndex, 1);
-    //     }
-
-    //     return monthWiseAmounts;
-    // }
-
-
-
-
-
-
-    // Challan Service
-
     async createChallan(challan: IChallan) {
         if (await this.challanExists(challan.challanNumber)) {
             throw new ApiError(statuscode.BADREQUEST, ERROR_MSG.EXISTS('Challan'));
@@ -287,10 +96,7 @@ export class ChallanService {
             siteName: challan.siteName
         });
 
-       
-
-        challanNumber == '0' ? await this.updateExistingBill(existingBill, billProducts)
-            : await billService.createBillBychallan(newChallan);
+        challanNumber == '1' ? await billService.createBillBychallan(newChallan) : await this.updateExistingBill(existingBill, existingBill?.products);
     }
 
     generateBillProducts(products: any, d: any) {
@@ -323,94 +129,94 @@ export class ChallanService {
         });
     }
 
-    async createNewBill(challan: IChallan, products: any, totalAmount: Number, challanId: string) {
-        const monthData = this.getMonthlyData(products);
+    // async createNewBill(challan: IChallan, products: any, totalAmount: Number, challanId: string) {
+    //     const monthData = this.getMonthlyData(products);
 
-        const aggregationPipeline = [
-            {
-              $facet: {
-                returnChallans: [
-                  {
-                    $match: {
-                      _id: [
-                              new mongoose.Schema.Types.ObjectId(
-                                challanId.toString()
-                              )
-                          ],
-                      challenType: "Return",
-                    },
-                  },
-                  {
-                    $lookup: {
-                      from: "customers",
-                      localField: "customerId",
-                      foreignField: "_id",
-                      as: "customerDetails",
-                    },
-                  },
-                  {
-                    $unwind: "$customerDetails",
-                  },
-                ],
-                deliveryChallan: [
-                  {
-                    $match: {
-                      _id: [
-                        new mongoose.Schema.Types.ObjectId(
-                          challanId.toString()
-                        )
-                    ],
-                      challenType: "Delivery",
-                    },
-                  },
-                  {
-                    $lookup: {
-                      from: "customers",
-                      localField: "customerId",
-                      foreignField: "_id",
-                      as: "customerDetails",
-                    },
-                  },
-                  {
-                    $unwind: "$customerDetails",
-                  },
-                ],
-              },
-            }
-          ]
-        const challans = await Challan.aggregate(aggregationPipeline);
-        const returnChallans = challans[0];
-        const deliveryChallans = challans[1];
+    //     const aggregationPipeline = [
+    //         {
+    //           $facet: {
+    //             returnChallans: [
+    //               {
+    //                 $match: {
+    //                   _id: [
+    //                           new mongoose.Schema.Types.ObjectId(
+    //                             challanId.toString()
+    //                           )
+    //                       ],
+    //                   challenType: "Return",
+    //                 },
+    //               },
+    //               {
+    //                 $lookup: {
+    //                   from: "customers",
+    //                   localField: "customerId",
+    //                   foreignField: "_id",
+    //                   as: "customerDetails",
+    //                 },
+    //               },
+    //               {
+    //                 $unwind: "$customerDetails",
+    //               },
+    //             ],
+    //             deliveryChallan: [
+    //               {
+    //                 $match: {
+    //                   _id: [
+    //                     new mongoose.Schema.Types.ObjectId(
+    //                       challanId.toString()
+    //                     )
+    //                 ],
+    //                   challenType: "Delivery",
+    //                 },
+    //               },
+    //               {
+    //                 $lookup: {
+    //                   from: "customers",
+    //                   localField: "customerId",
+    //                   foreignField: "_id",
+    //                   as: "customerDetails",
+    //                 },
+    //               },
+    //               {
+    //                 $unwind: "$customerDetails",
+    //               },
+    //             ],
+    //           },
+    //         }
+    //       ]
+    //     const challans = await Challan.aggregate(aggregationPipeline);
+    //     const returnChallans = challans[0];
+    //     const deliveryChallans = challans[1];
 
-        const deliveryProducts = deliveryChallans.map((ch:IChallan)=>{
-          return {...ch.products}
-        });
+    //     const deliveryProducts = deliveryChallans.map((ch:IChallan)=>{
+    //       return {...ch.products}
+    //     });
 
         
 
-        await Bills.create({
-            customerName: challanDetail[0].customerDetails.customerName,
-            mobileNumber: challanDetail[0].customerDetails.mobileNumber,
-            billNumber: 1,
-            damageCharge: challanDetail[0].damageCharge,
-            serviceCharge: challanDetail[0].serviceCharge,
-            billName: challanDetail[0].customerDetails.customerName,
-            billAddress: challanDetail[0].customerDetails.billingAddress || 'local',
-            partnerName: challanDetail[0].customerDetails.partnerName || '',
-            partnerMobileNumber: challanDetail[0].customerDetails.partnerMobileNumber || '',
-            date: challanDetail[0].date,
-            billTo: challanDetail[0].customerDetails.customerName,
-            reference: challanDetail[0].customerDetails.reference,
-            referenceMobileNumber: challanDetail[0].customerDetails.referenceMobileNumber,
-            siteName: challanDetail[0].siteName,
-            siteAddress: challanDetail[0].siteAddress,
-            pancard: challanDetail[0].customerDetails.pancard || 'id NULL',
-            products,
-            totalPayment: totalAmount,
-            challans: [challanId],
-            monthData
-        });
-    }
+    //     await Bills.create({
+    //         customerName: challanDetail[0].customerDetails.customerName,
+    //         mobileNumber: challanDetail[0].customerDetails.mobileNumber,
+    //         billNumber: 1,
+    //         damageCharge: challanDetail[0].damageCharge,
+    //         serviceCharge: challanDetail[0].serviceCharge,
+    //         billName: challanDetail[0].customerDetails.customerName,
+    //         billAddress: challanDetail[0].customerDetails.billingAddress || 'local',
+    //         partnerName: challanDetail[0].customerDetails.partnerName || '',
+    //         partnerMobileNumber: challanDetail[0].customerDetails.partnerMobileNumber || '',
+    //         date: challanDetail[0].date,
+    //         billTo: challanDetail[0].customerDetails.customerName,
+    //         reference: challanDetail[0].customerDetails.reference,
+    //         referenceMobileNumber: challanDetail[0].customerDetails.referenceMobileNumber,
+    //         siteName: challanDetail[0].siteName,
+    //         siteAddress: challanDetail[0].siteAddress,
+    //         pancard: challanDetail[0].customerDetails.pancard || 'id NULL',
+    //         products,
+    //         totalPayment: totalAmount,
+    //         challans: [challanId],
+    //         monthData
+    //     });
+    // }
 
     getMonthlyData(products: any) {
         let previousRestBill = 0;
